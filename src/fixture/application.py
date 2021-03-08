77663828136f17d5343a208e16cc61b9862ebb4b
lukasz-jab <lukasz.j.pl@interia.pl>
2021-03-08 18:42:40 +0100

    Created test for adding new group

--- src/fixture/application.py
+++ src/fixture/application.py
@@ -1,10 +1,14 @@
 from pywinauto.application import Application as WinApplication
 
+from src.fixture.group import GroupHelper
+
+
 class Application:
     def __init__(self, target):
         self.application = WinApplication().start(target)
         self.main_window = self.application.window(title="Free Address Book")
         self.main_window.wait("visible")
+        self.groups = GroupHelper(self)
 
     def destroy(self):
         self.main_window.close()
--- src/test/test_add_group.py
+++ src/test/test_add_group.py
@@ -1,3 +1,8 @@
 
 def test_add_group(app):
-    pass
+    old_groups = app.groups.get_group_list()
+    app.groups.add_group("Group Test")
+    new_groups = app.groups.get_group_list()
+    old_groups.append("Group Test")
+
+    assert sorted(new_groups) == sorted(old_groups)
